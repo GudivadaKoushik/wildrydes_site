c7a660ade411760731d26904445607cc81ea2fb1
GudivadaKoushik <163334988+GudivadaKoushik@users.noreply.github.com>
2025-10-15 00:45:30 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_XZx7nAllF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '65cua59h5g23rht602f7s1jj2i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_6jRw1Nubm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3017q0njgo17277fgk2qc4bem4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
